7ff79e3de24380e3589f7c810e04b7ec56c8a4f8
arashrahmani <arash.rahmani.999@gmail.com>
2021-05-08 14:08:11 +0430

    clean extra comments at humanoid_soccer_dataset module

--- humanoid_soccer_dataset/humanoid_soccer_dataset.py
+++ humanoid_soccer_dataset/humanoid_soccer_dataset.py
@@ -20,31 +20,6 @@ It should also contain any processing which has been applied (if any),
 # TODO(humanoid_soccer_dataset): BibTeX citation
 _CITATION = """
 """
-CLASSES_NUMBER = 6
-
-palette = np.array([
-    [  0.],
-    [  1.],
-    [  2.],
-    [  3.],
-    [  4.],
-    [  5.],
-    ], dtype=np.float32)
-palette = np.array([
-    [180.,120.,31. ] ,
-    [25.,176.,106. ] ,
-    [235.,62.,156. ] ,
-    [255.,255.,255.] ,
-    [232.,144.,69. ] ,
-    [28.,26.,227.  ] ,
-    # [31.,120.,180. ] ,
-    # [106.,176.,25. ] ,
-    # [156.,62.,235. ] ,
-    # [255.,255.,255.] ,
-    # [69.,144.,232. ] ,
-    # [227.,26.,28.  ] ,
-    ], dtype=np.float32)
-
 
 class HumanoidSoccerDataset(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for humanoid_soccer_dataset dataset."""
@@ -76,37 +51,20 @@ class HumanoidSoccerDataset(tfds.core.GeneratorBasedBuilder):
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
-    # TODO(humanoid_soccer_dataset): Downloads the data and defines the splits
-    # archive_path = dl_manager.manual_dir / 'humanoid_Dataset.zip'
-    
-    # for remote chooses
+    # TODO(humanoid_soccer_dataset): Downloads the data and defines the splits    
+
+    """for remote chooses"""
     # extracted_path = dl_manager.download_and_extract('http://data.org/data.zip')
-    # for local chooses
-    # extracted_path = dl_manager.extract('Copy_Dataset.zip')
+    
+    """for local chooses"""
     extracted_path = dl_manager.extract('/home/arash/fun/Humanoid-Robots-Semantic-Segmentation/humanoid_soccer_dataset/real_dataset.zip')
 
-    print(extracted_path)
     # TODO(humanoid_soccer_dataset): Returns the Dict[split names, Iterator[Key, Example]]
-
     return {
         'train': self._generate_examples(images_path = extracted_path / 'real_dataset/train/image',label_path = extracted_path / 'real_dataset/train/label'),
         'test': self._generate_examples(images_path = extracted_path / 'real_dataset/test/image',label_path = extracted_path / 'real_dataset/test/label'),
     }
-  def _one_hot_encode(self,y):
-    """Converts mask to a one-hot encoding specified by the semantic map."""
-    img = cv2.imread(str(y), cv2.IMREAD_COLOR)
-    semantic_map = []
-    for color in palette:
-      class_map = tf.reduce_all(tf.equal(img, color), axis=-1)
-      semantic_map.append(class_map)
-    semantic_map = tf.stack(semantic_map, axis=-1)
-    semantic_map = tf.cast(semantic_map, tf.float32)
-    semantic_map = np.array(semantic_map)
     
-    
-    
-    magic_number = tf.reduce_sum(semantic_map)
-    return semantic_map
   def _generate_examples(self, images_path, label_path):
     """Yields examples."""
     # TODO(humanoid_soccer_dataset): Yields (key, example) tuples from the dataset
@@ -115,9 +73,6 @@ class HumanoidSoccerDataset(tfds.core.GeneratorBasedBuilder):
       l = Path(os.path.join(label_path,img_name))
       f = cv2.resize(cv2.imread(str(f)), (320,240))
       l = cv2.resize(cv2.imread(str(l)), (320,240), 0, 0, interpolation=cv2.INTER_NEAREST)
-      # y = self._one_hot_encode(str(l))
-      # count = count + 1
-      # print("asdfasdfasdfa",type(y))
       yield str(f),{
         'image': f,
         'label': l,
@@ -127,9 +82,6 @@ class HumanoidSoccerDataset(tfds.core.GeneratorBasedBuilder):
       l = Path(os.path.join(label_path,img_name[:-4]+'.png'))
       f = cv2.resize(cv2.imread(str(f)), (320,240))
       l = cv2.resize(cv2.imread(str(l)), (320,240), 0, 0, interpolation=cv2.INTER_NEAREST)
-      # y = self._one_hot_encode(str(l))
-      # count = count + 1
-      # print("asdfasdfasdfa",type(y))
       yield str(f),{
         'image': f,
         'label': l,
